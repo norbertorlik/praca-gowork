37c497b4f7451c01701c9c7ad703f1eadada2e5c
norbertorlik <80628146+norbertorlik@users.noreply.github.com>
2023-06-29 14:42:13 +0200

    Add files via upload

---
+++ szyfrcezara.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cezar
+{
+    class Program
+    {
+        private static string _alphabet = "aąbcćdeęfghijklłmnńoópqrsśtuvwxyzźż";
+
+        public static void Main()
+        {
+            Console.WriteLine("Wpisz tekst:");
+            var input = Console.ReadLine();
+
+            var result = "";
+            foreach (var c in input)
+            {
+                var index = _alphabet.IndexOf(c);
+                result += _alphabet[(index + 13) % 35];
+            }
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
